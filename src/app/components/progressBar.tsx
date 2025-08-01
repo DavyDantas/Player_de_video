2fbfc4fda99c9b8c15d5a327136f15176e562cc1
Davy Dantas <davy.dantas@escolar.ifrn.edu.br>
2025-08-01 16:41:00 -0300

    player funcionando

--- src/app/components/progressBar.tsx
+++ src/app/components/progressBar.tsx
@@ -1,17 +1,95 @@
+import { useEffect, useRef, useState } from "react"
+
 interface props {
     progress: number,
-    duration: number
+    duration: number,
+    newPosition: (position:number) => void
 }
 
-export default function ProgressBar({progress, duration}: props){
+export default function ProgressBar({progress, duration, newPosition}: props){
+    const [isDragging, setIsDraging] = useState(false)
+    const [position, setPosition] = useState({x:0})
+    const drgaRef = useRef<HTMLDivElement>(null)
+    const initialMousePos = useRef({x:0})
+    const startEndBar = useRef<HTMLDivElement>(null)
+    const [durationFormated, setDurationFormated] = useState('')
+    const [progressFormated, setProgressFormated] = useState('')
+
+    const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+        if(startEndBar.current) {
+            const barProgress = startEndBar.current.getBoundingClientRect()
+            let axyX = e.clientX - barProgress.left
+            const newX = Math.max(0, Math.min(axyX, barProgress.width))
+
+            const newProgress = (newX / barProgress.width) * 100;
+            newPosition(newProgress);
+        }
+        setIsDraging(true)
+        initialMousePos.current = {x: e.clientX}
+    }
+
+    const handleMouseMove = (e:MouseEvent) => {
+        if(!isDragging) return
+        e.preventDefault()
+        if(startEndBar.current) {
+            const barProgress = startEndBar.current.getBoundingClientRect()
+            let axyX = e.clientX - barProgress.left
+            const newX = Math.max(0, Math.min(axyX, barProgress.width))
+
+            const newProgress = (newX / barProgress.width) * 100;
+            newPosition(newProgress);
+        }
+    }
+
+    const handleMouseUp = () => {
+        setIsDraging(false)
+    }
+
+    const calcTimeFormated = (time:number) => {
+        const hour = Math.floor(time / 3600)
+        const minutes = Math.floor((time % 3600) / 60)
+        const seconds = time % 60
+
+        const hourFormated = String(hour).padStart(2, '0')
+        const minutesFormated = String(minutes).padStart(2, '0')
+        const secondsFormated = String(seconds).padStart(2, '0')
+
+        return `${hour ? hourFormated + ':' : ''}${minutesFormated}:${secondsFormated}`
+    }
+
+    useEffect(() => {
+        setDurationFormated(calcTimeFormated(duration))
+    }, [duration])
+
+    useEffect(() => {
+        const secondsProgress = Math.floor((progress * duration) / 100)
+        setProgressFormated(calcTimeFormated(secondsProgress))
+    }, [progress])
+
+    useEffect(() => {
+        if(isDragging) {
+            document.addEventListener("mouseup", handleMouseUp)
+            document.addEventListener("mousemove", handleMouseMove)
+        }
+        return () => {
+            document.removeEventListener("mouseup", handleMouseUp)
+            document.removeEventListener("mousemove", handleMouseMove)
+        }
+    }, [isDragging, handleMouseMove, handleMouseUp])
+
     return (
-    <div className="z-20 bottom-[10px] w-[96%] absolute flex flex-col justify-center items-start">
-        <div className="backdrop-blur-2xl rounded-full h-[4px] w-full bg-[#ffffff25] flex items-center">
+    <div className="flex flex-col justify-center items-start">
+        <div onMouseDown={handleMouseDown} ref={startEndBar} id="full-bar" className="select-none z-20 backdrop-blur-2xl relative rounded-full h-[4px] w-full bg-[#ffffff71] flex items-center">
             <div
-             style={{width:`${progress}%`}}
-             id="bar-progress" className={`transition-all duration-100 ease-linear rounded-full z-20 h-[4px] bg-[#006eff]`}></div>
+             style={{width:`${progress}%`, userSelect: 'none'}}
+             id="bar-progress" className={`transition-all duration-100 ease-linear rounded-full z-20 h-[4px] bg-[#00c3ff]`}></div>
+            <div
+             id="point-bar"
+             ref={drgaRef}
+             style={{left: `${progress}%`, userSelect: 'none', position: 'absolute', transform: isDragging ? `translateX(${position.x}px)` : 'none'}}
+             className={`z-20 transition-all duration-100 ease-linear rounded-full w-[14px] h-[14px] bg-[#00c3ff]`}></div>
         </div>
-        <p className="mt-2 font-medium">{Math.floor((progress * duration) / 100) } / {duration}</p>
+        <p className="mt-3 text-[14px] font-bold">{progressFormated} / {durationFormated}</p>
     </div>
 )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { useRef, useState } from "react";
 export default function Home() {
   const [progressVideo, setProgress] = useState<number>(0)
   const videoRef = useRef<HTMLVideoElement>(null)
+  const [widthVideo, setWidthVideo] = useState(50)
+
   const handlePause = () => {
     const video = videoRef.current;
     if (!video) return;
@@ -15,7 +17,6 @@ export default function Home() {
       video.pause();
     }
   };
-
 
   const handleProgressTime = () => {
     const video = videoRef.current
@@ -29,19 +30,43 @@ export default function Home() {
     return video.duration
   }
 
+  const newProgress = (newPos:number) => {
+    setProgress(newPos)
+    const video = videoRef.current
+    if (!video) return
+    video.currentTime = (video.duration * newPos) / 100
+  }
+
+  const handleToggleWidth = () => {
+    setWidthVideo((width) => width === 50 ? 95 : 50);
+    console.log(widthVideo)
+  }
+
   return (
     <main className="items-center justify-center flex flex-col h-screen w-full">
-        <div className="items-center justify-center flex">
-            <div id="player" onClick={handlePause} className="relative z-10 bg-[#202020] rounded-xl h-[664px] w-[1180px] flex items-center justify-center">
+        <div style={{width: `${widthVideo}%`}}  className="transition-all duration-100 ease-linear relative z-10 bg-[#202020] rounded-xl flex items-center justify-center">
+            <div id="player" onClick={handlePause} className={`transition-all duration-100 ease-linear relative z-10 bg-[#202020] rounded-xl h-fit flex items-center justify-center`}>
               <video
                 src="/videos/video.mp4"
-                className="h-full w-auto object-cover rounded-xl"
+                className={`h-auto w-full object-cover ${widthVideo === 50 ? 'rounded-xl':''}`}
                 ref={videoRef}
                 onTimeUpdate={handleProgressTime}
                 autoPlay
               />
-              
-              <ProgressBar duration={durationVideo() ?? 0} progress={progressVideo}/>
+            </div>
+
+            <div className="z-20 bottom-[14px] w-[96%] absolute ">
+              <button
+                className="p-2 cursor-pointer absolute right-[-8px] top-[8px] z-30"
+                onClick={handleToggleWidth}
+                title="Toggle video width"
+                aria-label="Toggle video width"
+                >
+                <div className="w-[36] h-[26] border-[3px] flex justify-center items-center border-white rounded-[2]">
+                  <div className="w-[16] h-[11] border-[3px] border-gray-200"></div>
+                </div>
+              </button>
+              <ProgressBar newPosition={newProgress} duration={durationVideo() ?? 0} progress={progressVideo}/>
             </div>
         </div>
     </main>
